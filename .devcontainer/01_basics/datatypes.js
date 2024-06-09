17345c670c03a1f75612e9473944e9b90e55f60c
krishnu88 <154278387+krishnu88@users.noreply.github.com>
2024-06-09 10:08:21 +0000

    datatypes

---
+++ .devcontainer/01_basics/datatypes.js
@@ -0,0 +1,22 @@
+"use strict";
+console.log("heloo today we start with data types")
+console.log(8+9)
+console.log(9+ //bad practise ;make your code readable 
+    90
+)
+//starting with datatypes
+//primitive datatype
+//symbol is also a primitive datatype
+let name="krish" //-->string datatype;collection of characters;we mention string inside "" or ''.
+let age=20; //-->number data type;numerical values; range (-(2^53-1) to (2^53-1))
+let isLoggedIn=true //boolean value; true or false
+let email;  //output is undefined;it is a datatype that tells that variable has been declared but not value is assigned to it yet
+let status=null;
+console.log(name);
+console.log(age);
+console.log(isLoggedIn)
+console.log(email)
+console.table([typeof name,typeof age,typeof isLoggedIn,typeof email,typeof status]);
+
+
+
